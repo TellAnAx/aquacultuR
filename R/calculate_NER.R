cd8e44a48ab63dc273b6fb1badcd7c7b9ee0fa51
Anil Tellbuescher <anil@tellbuescher.online>
2024-10-09 09:26:06 +0200

    created generic version of protein efficiency ratio

---
+++ R/calculate_NER.R
@@ -0,0 +1,13 @@
+calculate_NER <- function(initWeight = NULL, finalWeight = NULL, weightGain = NULL, proteinFed = NULL) {
+  
+  if (((is.null(initWeight) | is.null(finalWeight)) & is.null(weightGain)) | is.null(proteinFed)) {
+    stop("The Protein Efficiency Ratio cannot be calculated. Necessary data is missing.")
+  }
+  
+  if(is.null(weightGain)) {
+    weightGain <- finalWeight - initWeight  
+  }
+  
+  result <- weightGain / proteinFed
+  return(result)
+}

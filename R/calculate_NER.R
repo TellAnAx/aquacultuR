b65db4dc62f265924b161bdcf2fa5940e3c91ec1
Anil Tellbuescher <anil@tellbuescher.online>
2024-10-09 09:29:35 +0200

    add comment

--- R/calculate_NER.R
+++ R/calculate_NER.R
@@ -1,3 +1,4 @@
+# Calculate nutrient efficiency ratio
 calculate_NER <- function(initWeight = NULL, finalWeight = NULL, weightGain = NULL, proteinFed = NULL) {
   
   if (((is.null(initWeight) | is.null(finalWeight)) & is.null(weightGain)) | is.null(proteinFed)) {

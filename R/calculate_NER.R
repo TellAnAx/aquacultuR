78bbbe9a2a8b4e8f62d71aefd1228aa484b4f5be
Anil Tellbuescher <anil@tellbuescher.online>
2024-10-21 22:51:25 +0200

    change wording and argument name from Protein to Nutrient, making it more generic

--- R/calculate_NER.R
+++ R/calculate_NER.R
@@ -1,8 +1,8 @@
 # Calculate nutrient efficiency ratio
-calculate_NER <- function(initWeight = NULL, finalWeight = NULL, weightGain = NULL, proteinFed = NULL) {
+calculate_NER <- function(initWeight = NULL, finalWeight = NULL, weightGain = NULL, nutrientFed = NULL) {
   
-  if (((is.null(initWeight) | is.null(finalWeight)) & is.null(weightGain)) | is.null(proteinFed)) {
-    stop("The Protein Efficiency Ratio cannot be calculated. Necessary data is missing.")
+  if (((is.null(initWeight) | is.null(finalWeight)) & is.null(weightGain)) | is.null(nutrientFed)) {
+    stop("The Nutrient Efficiency Ratio cannot be calculated. Necessary data is missing.")
   }
   
   if(is.null(weightGain)) {

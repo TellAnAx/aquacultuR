5abfa155d3f64ef117e8dd69897fc2ba2c7a3cac
Anil Tellbuescher <anil@tellbuescher.online>
2025-07-22 10:51:06 +0200

    add dm_diet to test and update remaining argument names

--- tests/testthat/test-adc_dm.R
+++ tests/testthat/test-adc_dm.R
@@ -1,5 +1,6 @@
 test_that("Apparent Digestibility calculation of Dry Matter works", {
-  expect_equal(adc_dm(es_diet = 0.01,
-                      es_feces = 0.05), 
+  expect_equal(adc_dm(dm_diet = 1,
+                      std_diet = 0.01,
+                      std_feces = 0.05), 
                0.8)
 })

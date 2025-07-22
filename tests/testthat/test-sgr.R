97044b23766c9dc489898030233486839356439f
Anil Tellbuescher <anil@tellbuescher.online>
2025-07-22 12:52:59 +0200

    add test from update_tests

--- tests/testthat/test-sgr.R
+++ tests/testthat/test-sgr.R
@@ -1,7 +1,15 @@
-test_that("Specific Growth Rate calculated correctly", {
-  expect_equal(sgr(ibw = 1, fbw = 2, duration = log(2)), 100)
-})
-
-test_that("Logarithmus naturalis calculated correctly", {
-  expect_equal(log(1), 0)
+test_that( "Specific growth rate",{
+  expect_equal(sgr(ibw=1, fbw=2, duration=3,1))
+  expect_warning(sgr(ibw=1, fbw=0, duration=3))
+  expect_warning(sgr(ibw=0,fbw=2,duration=3))
+  expect_warning(sgr(ibw=-1,fbw=2,duration=3))
+  expect_warning(sgr(ibw=1,fbw=-2,duration=3))
+  expect_warning(sgr(ibw=1,fbw=2,duration=-3))
+  expect_error(sgr(ibw=1,fbw=2,duration=0))
+  expect_error(sgr(ibw= 'test',fbw=2,duration=3))
+  expect_error(sgr(ibw=1,fbw= 'test',duration=3))
+  expect_error(sgr(ibw=1,fbw=2,duration= 'test'))
+  expect_error(sgr(ibw=na,fbw=2,duration=3))
+  expect_error(sgr(ibw=1,fbw=na,duration=3))
+  expect_error(sgr(ibw=1,fbw=2,duration=na))
 })

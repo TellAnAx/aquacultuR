e54907fbcad4c7deef8d9a386d030c23094a60f9
Madhav Karthikeyan <madhavleo2000@gmail.com>
2025-07-25 07:02:05 +0200

    Updated testthat function

--- R/rfr.R
+++ R/rfr.R
@@ -15,7 +15,28 @@
 #' @export
 rfr <- function(m_feed = NULL, 
                 m_bio = NULL,
-               duration = NULL) {
+                duration = NULL) {
+  # Error checks
+  if (any(is.na(c(m_feed, m_bio, duration)))) 
+    stop("Inputs cannot be NA")
+  
+  if (!is.numeric(m_feed) || !is.numeric(m_bio) || !is.numeric(duration)) 
+    stop("All inputs must be numeric")
+  
+  if (duration == 0) 
+    stop("Duration cannot be zero")
+  
+  
+  # Warnings
+  if (m_feed <= 0) 
+    warning("m_feed is zero or negative")
+  
+  if (m_bio <= 0) 
+    warning("m_bio is zero or negative")
+  
+  if (duration < 0) 
+    warning("duration is negative")
+  
   stopifnot(!is.null(m_feed) | !is.null(m_bio) | !is.null(duration))
   
   rfr <- m_feed / (m_bio * duration)

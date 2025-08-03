d48ad6820d5b700fd0c7fa69969c15b4db56cb6c
Madhav Karthikeyan <madhavleo2000@gmail.com>
2025-08-03 07:03:32 +0200

    Sample on Vignette, just have to input formulas and change the values based on actual studies, rather than random numbers

--- tests/testthat/test-fr.R
+++ tests/testthat/test-fr.R
@@ -1,10 +1,10 @@
 test_that( "feeding rate",{
   expect_error(fr( m_feed=1, duration=0))
-  expect_warning(fr(m_feed=0,duration=1))
+  expect_error(fr(m_feed=0,duration=1))
   expect_error(fr(m_feed=na, duration=2))
   expect_error(fr(m_feed=1, duration=na))
   expect_error(fr(m_feed= 'test', duration=2))
   expect_error(fr(m_feed=1, duration= 'test'))
-  expect_warning(fr(m_feed=-1, duration= 2))
+  expect_error(fr(m_feed=-1, duration= 2))
   expect_error(fr(m_feed=1, duration=-2))
 })

3cbab8d66af0c3d393082865a1e02fb985a715e2
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-06 17:38:47 +0200

    update function arguments; reorder and add tests

--- tests/testthat/test-agr.R
+++ tests/testthat/test-agr.R
@@ -1,16 +1,28 @@
-test_that('Absolute growth rate',{
-  
-  expect_warning(agr(m_start=1, m_end=0, duration=3))
-  expect_warning(agr(m_start=0,m_end=2,duration=3))
-  expect_error(agr(m_start=-1,m_end=2,duration=3))
-  expect_error(agr(m_start=1,m_end=-2,duration=3))
-  expect_error(agr(m_start=1,m_end=2,duration=-3))
-  expect_warning(agr(m_start=1,m_end=2,duration=0))
-  expect_error(agr(m_start= 'test',m_end=2,duration=3))
-  expect_error(agr(m_start=1,m_end= 'test' ,duration=3))
-  expect_error(agr(m_start=1,m_end=2,duration= 'test'))
-  expect_error(agr(m_start=na,m_end=2,duration=3))
-  expect_error(agr(m_start=1,m_end=na,duration=3))
-  expect_error(agr(m_start=1,m_end=2,duration=na))
+test_that("agr() throws warning when 0 is provided", {
+  expect_warning(agr(ibw = 0, fbw = 2, duration = 3))
+  expect_warning(agr(ibw = 1, fbw = 0, duration = 3))
+  expect_warning(agr(ibw = 1, fbw = 2, duration = 0))
+})
+
+test_that("agr() throws error when negative value is provided",{
+  expect_error(agr(ibw = -1,fbw = 2,duration = 3))
+  expect_error(agr(ibw = 1,fbw = -2,duration = 3))
+  expect_error(agr(ibw = 1,fbw = 2,duration = -3))
+})
+
+test_that("agr() throws error when character is provided", {
+  expect_error(agr(ibw =  "test", fbw = 2, duration = 3))
+  expect_error(agr(ibw = 1, fbw =  "test" , duration = 3))
+  expect_error(agr(ibw = 1, fbw = 2, duration =  "test"))
+})
+
+test_that("agr() throws error when NA is provided", {
+  expect_error(agr(ibw = NA, fbw = 2, duration = 3))
+  expect_error(agr(ibw = 1, fbw = NA, duration = 3))
+  expect_error(agr(ibw = 1, fbw = 2, duration = NA))
+})
+
+test_that("agr() calculates absolute growth correctly", {
+  expect_equal(agr(ibw = 1, fbw = 2, duration = 10), 1/10)
 })
 

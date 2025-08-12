77a78475de3c5ed8b3d122e9ccce335553d585c1
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-12 09:19:12 +0200

    replace m_bio by ag, according to the ag() function

--- R/rfr.R
+++ R/rfr.R
@@ -5,8 +5,8 @@
 #' 
 #' @param m_feed numeric; total amount of feed given in grams over the time 
 #' period of interest. 
-#' @param m_bio numeric value; biomass increase.
-#' @param duration numeric; time period of interest 
+#' @param ag numeric value; absolute growth in grams during the experimental period.
+#' @param duration numeric; time period of interest in days.
 #' 
 #' @return numeric; multiply by 100 for conversion into percentage.
 #' 
@@ -14,31 +14,31 @@
 #' 
 #' @export
 rfr <- function(m_feed = NULL, 
-                m_bio = NULL,
+                ag = NULL,
                 duration = NULL) {
   
   # Checks----
   ## Ensure inputs are numeric
-  stopifnot("Error: Inputs must be numeric!" = is.numeric(m_feed), is.numeric(m_bio), is.numeric(duration))
+  stopifnot("Error: Inputs must be numeric!" = is.numeric(m_feed), is.numeric(ag), is.numeric(duration))
   
   ## Ensure inputs have the same length
-  if (length(m_feed) != length(m_bio)) {
-    stop("Error: 'm_feed' and 'm_bio' must be of the same length.")
+  if (length(m_feed) != length(ag)) {
+    stop("Error: 'm_feed' and 'ag' must be of the same length.")
   }
   
   ## Check whether inputs are != 0
-  if (any(m_bio == 0 | duration == 0)) {
-    stop("Error: 'm_bio' and/or 'duration' is equal to zero. The result cannot be calculated.")
+  if (any(ag == 0 | duration == 0)) {
+    stop("Error: 'ag' and/or 'duration' is equal to zero. The result cannot be calculated.")
   }
   
   ## Check whether inputs are < 0
-  if (any(m_feed <0 | m_bio < 0 | duration < 0)) {
+  if (any(m_feed <0 | ag < 0 | duration < 0)) {
     warning("Warning: One or many inputs are smaller than zero. The result is not meaningful.")
   }
   
   
   
   # Calculations----
-  rfr <- m_feed / (m_bio * duration)
+  rfr <- m_feed / (ag * duration)
   return(rfr)
 }

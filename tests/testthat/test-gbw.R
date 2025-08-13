e95cd5456d6a0a0040af9cde42cc5b28d35f56f4
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-13 12:17:13 +0200

    adjust test sequence according to checks in function script

--- tests/testthat/test-gbw.R
+++ tests/testthat/test-gbw.R
@@ -1,22 +1,26 @@
-test_that("gbw() throws an error if inputs are negative",{
-  expect_error(gbw(ibw  = -1, fbw = 2))
-  expect_error(gbw(ibw = 1, fbw = -2))
-})
-
 test_that("gbw() throws an error if inputs are NA", {
   expect_error(gbw(ibw = NA,fbw = 2))
   expect_error(gbw(ibw = 1, fbw = NA))
 })
+
 
 test_that("gbw() throws an error if inputs are non-numeric", {
   expect_error(gbw(ibw =  "test", fbw = 2))
   expect_error(gbw(ibw = 1, fbw = "test"))
 })
 
-test_that("gbw() throws an error if inputs are not of the same length", {
-  expect_error(gbw(ibw =  c(1,2), fbw = 2))
-  expect_error(gbw(ibw = 1, fbw = c(1,2)))
+
+test_that("gbw() throws an error if inputs are negative",{
+  expect_error(gbw(ibw  = -1, fbw = 2))
+  expect_error(gbw(ibw = 1, fbw = -2))
 })
+
+
+test_that("gbw() throws a message if inputs are not of the same length", {
+  expect_message(gbw(ibw =  c(1,2), fbw = 2))
+  expect_message(gbw(ibw = 1, fbw = c(1,2)))
+})
+
 
 test_that("gbw() calculates the Geometric Bodyweight correctly", {
   expect_equal(gbw(ibw = 1, fbw = 4), 2)

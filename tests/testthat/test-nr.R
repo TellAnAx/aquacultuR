9e81587bbdae2d8aeee29e4e8dc70b025778a767
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-14 17:11:33 +0200

    update and expand tests

--- tests/testthat/test-nr.R
+++ tests/testthat/test-nr.R
@@ -1,21 +1,36 @@
-test_that( "nutrient nr", {
-  
-  expect_warning(nr(ibw=-1, fbw= 2, nut_start=3,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= -2, nut_start=3,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= 2, nut_start=-3,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= 2, nut_start=3,nut_end=-4))
-  expect_warning(nr(ibw=0, fbw= 2, nut_start=3,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= 0, nut_start=3,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= 2, nut_start=0,nut_end=4))
-  expect_warning(nr(ibw=1, fbw= 2, nut_start=3,nut_end=0))
-  expect_error(nr(ibw= 'test', fbw= 2, nut_start=3,nut_end=4))
-  expect_error(nr(ibw=1, fbw= 'test', nut_start=3,nut_end=4))
-  expect_error(nr(ibw=1, fbw= 2, nut_start= 'test',nut_end=4))
-  expect_error(nr(ibw=1, fbw= 2, nut_start=3,nut_end= 'test'))
-  expect_error(nr(ibw=NA, fbw= 2, nut_start=3,nut_end=4))
-  expect_error(nr(ibw=1, fbw= NA, nut_start=3,nut_end=4))
-  expect_error(nr(ibw=1, fbw= 2, nut_start=NA,nut_end=4))
-  expect_error(nr(ibw=1, fbw= 2, nut_start=3,nut_end=NA))
-  expect_error(nr(ibw> fbw, nut_start=3,nut_end=4))
-  expect_error(nr(ibw=1, fbw= 2, nut_start<nut_end))
+test_that("nr() throws error when input is NA.", {
+  expect_error(nr(ibw = NA, fbw = 2, ibn = 0.1, fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = NA, ibn = 0.1, fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = 2, ibn = NA, fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = 2, ibn = 0.1, fbn = NA))
+})
+
+
+test_that("nr() throws an error when input is non-numeric.", {
+  expect_error(nr(ibw = "test", fbw = 2, ibn = 0.1, fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = "test", ibn = 0.1, fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = 2, ibn = "test", fbn = 0.2))
+  expect_error(nr(ibw = 1, fbw = 2, ibn = 0.1, fbn = "test"))
+})
+
+
+test_that("nr() throws warning when inputs are out of range.", {
+  expect_warning(nr(ibw = 0, fbw = 2, ibn = 0.1, fbn = 0.2))
+  expect_warning(nr(ibw = 1, fbw = 0, ibn = 0.1, fbn = 0.2))
+  expect_warning(nr(ibw = 1, fbw = 2, ibn = -0.1, fbn = 0.2))
+  expect_warning(nr(ibw = 1, fbw = 2, ibn = 0.1, fbn = 1.2))
+})
+
+
+test_that("nr() throws message when inputs differ in length.", {
+  expect_message(nr(ibw = c(1,1), fbw = 2, ibn = 0.1, fbn = 0.2))
+  expect_message(nr(ibw = 1, fbw = c(2,2), ibn = 0.1, fbn = 0.2))
+  expect_message(nr(ibw = 1, fbw = 2, ibn = c(0.1,0.1), fbn = 0.2))
+  expect_message(nr(ibw = 1, fbw = 2, ibn = 0.1, fbn = c(0.2,0.2)))
+})
+
+
+test_that("nr() calculates result correctly.", {
+  expect_equal(nr(ibw = 1, fbw = 2, ibn = 0.1, fbn = 0.2), 0.3)
+  expect_message(expect_equal(nr(ibw = 1, fbw = c(2,3), ibn = 0.1, fbn = c(0.3, 0.4)), c(0.5, 1.1)))
 })

ba3589d4c7bd82b4b8f54a63796d52a39000d5de
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-16 22:24:11 +0200

    change feed to fi and update examples

--- R/nr.R
+++ R/nr.R
@@ -8,7 +8,7 @@
 #' of the experimental animal. The value must be between 0 and 1.
 #' @param fbn numeric; final mass fraction of the target nutrient in the tissue 
 #' of the experimental animal. The value must be between 0 and 1.
-#' @param feed numeric; mass of feed given
+#' @param fi numeric; mass of feed given
 #' @param nut_diet numeric; mass fraction of nutrient in the diet. The value must
 #' be between 0 and 1.
 #' 
@@ -19,15 +19,22 @@
 #' 221–229 (2024). https://doi.org/10.1038/s43016-024-00932-z
 #' 
 #' @examples
-#' # initial bodyweight = 10 g
-#' # final bodyweight = 100 g
-#' # initial mass fraction of N = 100 g/kg (10%)
-#' # final mass fraction of N = 150 g/kg (15%)
-#' # -> 14 g N have been taken up.
-#' nr(ibw = 10, fbw = 100, ibn = 0.1, fbn = 0.15)
+#' # initial bodyweight = 10 g (0.01 kg)
+#' # final bodyweight = 100 g (0.1 kg)
+#' # initial mass fraction of N = 128 g/kg (80% CP on DM basis)
+#' # final mass fraction of N = 132 g/kg (82.5% CP on DM basis)
+#' # feed intake = 200 g (0.2 kg)
+#' # N in feed = 72 g/kg (45% CP)
+#' 
+#' nr(10, 100, 0.128, 0.132, 200, 0.072) * 100
+#' 
+#' # 82.7% of the provided N has been retained
+#' 
 #' 
 #' # multiple fish
-#' nr(ibw = 10, fbw = c(93, 102, 99, 98, 101, 132), ibn = 0.1, fbn = 0.15)
+#' nr(ibw = 10, fbw = c(93, 102, 99, 98, 101, 132), 
+#'    ibn = 0.128, fbn = 0.132, 
+#'    fi = 200, nut_diet = 0.072)
 #' 
 #' 
 #' @author Anıl Axel Tellbüscher
@@ -37,22 +44,22 @@ nr <- function(ibw,
                fbw,
                ibn,
                fbn,
-               feed,
+               fi,
                nut_diet) {
 
   # Checks----
   ## Check whether inputs are NA
-  if (any(is.na(c(ibw, fbw, ibn, fbn, feed, nut_diet)))) 
+  if (any(is.na(c(ibw, fbw, ibn, fbn, fi, nut_diet)))) 
     stop("Inputs must not be NA!")
 
 
   ## Check whether inputs are non-numeric
-  if (any(!is.numeric(c(ibw, fbw, ibn, fbn, feed, nut_diet))))
+  if (any(!is.numeric(c(ibw, fbw, ibn, fbn, fi, nut_diet))))
     stop("Inputs must be numeric!")
   
   
   ## Check whether feed|nut_diet == 0
-  if(any(feed == 0 | nut_diet == 0))
+  if(any(fi == 0 | nut_diet == 0))
     stop("Input must not be zero! Result cannot be calculated.")
 
 
@@ -61,8 +68,8 @@ nr <- function(ibw,
     warning("ibw is <= 0! The result is not meaningful.")
   if (any(fbw <= 0)) 
     warning("fbw is <= 0! The result is not meaningful.")
-  if (any(feed < 0)) 
-    warning("feed is < 0! The result is not meaningful.")
+  if (any(fi < 0)) 
+    warning("fi is < 0! The result is not meaningful.")
   if (any(ibn < 0 | ibn > 1)) 
     warning("ibn is out of range! The result is not meaningful.")
   if (any(fbn < 0 | fbn > 1)) 
@@ -73,14 +80,14 @@ nr <- function(ibw,
 
   ## Check whether inputs are of same length
   length_ratio <- c(length(ibw), length(fbw), length(ibn), 
-                    length(fbn), length(feed), length(nut_diet)) / length(ibw)
+                    length(fbn), length(fi), length(nut_diet)) / length(ibw)
   if(any(length_ratio != 1))
     message("Inputs differ in length.")
 
 
 
   # Calculations----
-  nr <- (fbw * fbn - ibw * ibn) / (feed * nut_diet)
+  nr <- (fbw * fbn - ibw * ibn) / (fi * nut_diet)
   
   return(nr)
 }

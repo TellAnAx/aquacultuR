ee1165a2978a86d65a0df7ab20ae1e00aa1c0d7e
Anil Tellbuescher <anil@tellbuescher.online>
2025-08-23 11:08:52 +0200

    update dataset and documentation

--- R/weight2.R
+++ R/weight2.R
@@ -1,14 +1,18 @@
-#' Fish biomass and bodyweights from the second polyculture experiment
+#' Individual fish bodyweights during a polyculture experiment
 #' 
-#' A dataset containing weights of fish species during a polyculture experiment.
+#' A dataset holding weight data of tagged individuals of two different fish 
+#' species, pikeperch (Sander lucioperca) and largemouth bass (Micropterus 
+#' salmoides), at the beginning and the end of a polyculture experiment for 
+#' aquaculture purposes.
 #' 
 #' @docType data
 #' @keywords datasets
 #' @name weight2
 #' @usage data(weight2)
 #' @references Pěnka T., Malinovskyi O., Křišťan J., Imentai A., Policar T. 
-#' (2021): Effect of density and mixed culture with pikeperch (Sander lucioperca) 
-#' on effectivity of largemouth bass (Micropterus salmoides) intensive culture. 
-#' Czech Journal of Animal Science, 66: 428–440.
-#' @format A data frame with 396 rows and 3 variables
+#' (2021): Effect of density and mixed culture with pikeperch (Sander 
+#' lucioperca) on effectivity of largemouth bass (Micropterus salmoides) 
+#' intensive culture. Czech Journal of Animal Science, 66: 428–440.
+#' @format A data frame with 4 variables ('species', 'culture_sys', 'ibw_g', 
+#' 'fbw_g') and 396 observations.  
 NULL

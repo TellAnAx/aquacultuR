2ef554eece4eb8608bc190ec1c2acdba4eb21231
Anil Tellbuescher <anil@tellbuescher.online>
2025-12-12 17:59:41 +0100

    create folder for all new data that will be introduced and ignore during package building

---
+++ data_prep/feed_intake.R
@@ -0,0 +1,45 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(purrr)
+
+# --- 1) Read the sheet without headers ---
+df_raw <- read_excel(
+  "data/Trial_A_data.xlsx", 
+  sheet = "FeedIntake", 
+  col_names = FALSE, 
+  skip = 2
+)
+
+
+# --- 2) Extract the first two header rows ---
+hdr1 <- df_raw %>% slice(1) %>% as.character() %>% print()
+hdr2 <- df_raw %>% slice(2) %>% as.character() %>% print()
+
+
+# --- 3) Forward-fill group labels across NAs (e.g., "Water temperature (°C)" across T1..T11) ---
+# Convert to tibble for fill, then back to vector
+hdr1_filled <- tibble(hdr1 = hdr1) %>%
+  tidyr::fill(hdr1, .direction = "down") %>%
+  pull(hdr1) %>% 
+  print()
+
+
+# --- 4) Build final names by combining hdr1_filled and hdr2 ---
+# For columns where hdr1_filled is NA/empty (like the date column),
+# just use hdr2; otherwise combine with an underscore.
+hdr1_filled[is.na(hdr1_filled)] <- ""
+final_names <- ifelse(
+  hdr1_filled == "",
+  hdr2,
+  paste0(hdr1_filled, "_", hdr2)
+) %>% 
+  print()
+
+
+# --- 5) Apply column names and drop the first two header rows ---
+df <- df_raw %>%
+  slice(-(1:2)) %>%            # remove header rows
+  setNames(final_names)
+
---
+++ data_prep/water_parameters.R
@@ -0,0 +1,55 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(purrr)
+    
+# --- 1) Read the sheet without headers ---
+df_raw <- read_excel(
+  "data/Trial_A_data.xlsx", 
+  sheet = "WaterParametersDaily", 
+  col_names = FALSE, 
+  skip = 1
+  )
+  
+
+# --- 2) Extract the first two header rows ---
+hdr1 <- df_raw %>% slice(1) %>% as.character() %>% print()
+hdr2 <- df_raw %>% slice(2) %>% as.character() %>% print()
+  
+
+# --- 3) Forward-fill group labels across NAs (e.g., "Water temperature (°C)" across T1..T11) ---
+# Convert to tibble for fill, then back to vector
+hdr1_filled <- tibble(hdr1 = hdr1) %>%
+  tidyr::fill(hdr1, .direction = "down") %>%
+  pull(hdr1) %>% 
+  print()
+  
+
+# --- 4) Build final names by combining hdr1_filled and hdr2 ---
+# For columns where hdr1_filled is NA/empty (like the date column),
+# just use hdr2; otherwise combine with an underscore.
+hdr1_filled[is.na(hdr1_filled)] <- ""
+final_names <- ifelse(
+  hdr1_filled == "",
+  hdr2,
+  paste0(hdr1_filled, "_", hdr2)
+  ) %>% 
+  print()
+  
+
+# --- 5) Apply column names and drop the first two header rows ---
+df <- df_raw %>%
+  slice(-(1:2)) %>%            # remove header rows
+  setNames(final_names)
+  
+
+# --- 6) Convert date
+df %>%
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30")) %>% 
+  pivot_longer(
+    cols = `Water temperature (°C)_T1`:last_col(),
+    names_to = c("parameter", "treatment"),
+    names_pattern = "^(.*)_(.*)$",
+    values_to = "value"
+    )

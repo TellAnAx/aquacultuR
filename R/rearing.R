e65a54fb97f9a685aa492568e634af3c411fc7ea
Anil Tellbuescher <anil@tellbuescher.online>
2025-12-16 09:38:20 +0100

    update documentation

--- R/rearing.R
+++ R/rearing.R
@@ -4,16 +4,19 @@
 #' temperature over all tanks stocked with pikeperch oder sturgeon. The 
 #' variables are as follows:
 #' 
-#' \itemize{
-#'    \item tank. unique identifier for each fish tank.
-#'    \item feed_g. feed input in gram over the duration of the experiment.
-#'    \item temp_C. water temperature in degrees Celsius.
-#'    \item duration. duration of the experiment.
+#' @format A data frame containing 24 rows and four columns:
+#' \describe{
+#'    \item{tank}{character; unique identifier for each fish tank.}
+#'    \item{feed_g}{numeric; total feed input in gram over the duration of the 
+#'    experiment.}
+#'    \item{temp_C}{numeric; average water temperature in degrees Celsius.}
+#'    \item{duration}{numeric; total duration of the experiment in days.}
 #' }
+#' 
+#' @examples
+#' rearing
 #' 
 #' @docType data
 #' @keywords datasets
 #' @name rearing
-#' @usage data(rearing)
-#' @format A data frame with 24 rows and 4 variables.
 NULL
--- R/weight.R
+++ R/weight.R
@@ -1,10 +1,24 @@
 #' Fish biomass and bodyweights from the first polyculture experiment
 #' 
-#' A dataset containing fish weights 
-#'
+#' A dataset containing fish weights from a polyculture experiment with 
+#' pikeperch (Sander lucioperca) and Russian sturgeon.
+#' 
+#' @format A tibble with 24 rows and eight columns:
+#' \describe{
+#'  \item{tank}{character; rearing tank identifier.}
+#'  \item{treat_id}{character; treatment identifier.}
+#'  \item{count_start}{numeric; fish count at the beginning of the experiment.}
+#'  \item{count_end}{numeric; fish count at the end of the experiment.}
+#'  \item{biomass_kg_start}{numeric; total fish biomass in kg at the beginning
+#'  of the experiment.}
+#'  \item{biomass_kg_end}{numeric; total fish biomass in kg at the end of the 
+#'  experiment.}
+#' }
+#' 
+#' @examples
+#' weight
+#' 
 #' @docType data
 #' @keywords datasets
 #' @name weight
-#' @usage data(weight)
-#' @format A data frame with 24 rows and 8 variables
 NULL
